3ed7dfe8e78f221dc44f3aff791086545812dcc3
Travis Brackett <travmeister@gmail.com>
2021-08-06 14:18:27 -0700

    Fixes

--- downloader/certificate.py
+++ downloader/certificate.py
@@ -6,8 +6,13 @@ def pkcs12_to_pem(data_b64):
   bundle_data = base64.b64decode(data_b64)
   bundle = pkcs12.load_key_and_certificates(bundle_data, None)
 
+  # Server cert
+  pem_data = bundle[1].public_bytes(
+    encoding=serialization.Encoding.PEM,
+  )
+
   # Private key
-  pem_data = bundle[0].private_bytes(
+  pem_data += bundle[0].private_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PrivateFormat.PKCS8,
     encryption_algorithm=serialization.NoEncryption()
@@ -16,11 +21,6 @@ def pkcs12_to_pem(data_b64):
   # Intermediate certs
   for c in bundle[2]:
     pem_data += c.public_bytes(
-    encoding=serialization.Encoding.PEM,
-  )
-
-  # Server cert
-  pem_data += bundle[1].public_bytes(
     encoding=serialization.Encoding.PEM,
   )
 
