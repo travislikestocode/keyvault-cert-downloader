17094cdbe6cbcef5c0a60ec485b633118009595e
Ubuntu <azureuser@lb.nfs1im030q2uhobdpmeof2l0ja.dx.internal.cloudapp.net>
2021-08-07 00:32:06 +0000

    Add hook

---
+++ downloader/hook.py
@@ -0,0 +1,25 @@
+from os import access
+import sys
+import dbus
+
+class Hook:
+  def __init__(self, action, service):
+    self.action = action
+    self.service = service + '.service'
+
+  def _systemd_action(self):
+    bus = dbus.SystemBus()
+    systemd = bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
+    manager = dbus.Interface(systemd, 'org.freedesktop.systemd1.Manager')
+
+    if self.action == 'reload':
+      managerMethod = manager.ReloadUnit
+    elif self.action == 'restart':
+      managerMethod = manager.RestartUnit
+    else:
+      sys.exit('Action must be "reload" or "restart"')
+
+    managerMethod(self.service, 'replace')
+
+  def run(self):
+    self._systemd_action()
--- downloader/main.py
+++ downloader/main.py
@@ -1,8 +1,13 @@
 from config import config
 from keyvault import Keyvault
 from output import Output
+from hook import Hook
 
 vault = Keyvault(list(config['keyvaults'].keys())[0])
 local_storage = Output(config['output']['dir'])
 
 local_storage.sync(vault.secrets)
+
+if config['hook']:
+  hook = Hook(config['hook']['action'], config['hook']['service'])
+  hook.run()
